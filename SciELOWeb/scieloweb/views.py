3399941a82b1c21e2b6a5ff0078551e7a5e8bf63
fabiobatalha <fabiobcs@yahoo.com.br>
2011-04-06 11:27:13 -0300

    Translations adjustments

--- SciELOWeb/scieloweb/views.py
+++ SciELOWeb/scieloweb/views.py
@@ -6,14 +6,13 @@ from pyramid.httpexceptions import HTTPFound
 from pyramid.renderers import get_renderer
 from pyramid.i18n import TranslationStringFactory, negotiate_locale_name, get_localizer
 
-
 _ = TranslationStringFactory('SciELOWeb')
 
 import urllib2
 import pdb
 import json
 
-from scieloweb import settings
+from scieloweb import settings_app
 
 def set_language(request):
     if request.POST:
@@ -22,25 +21,32 @@ def set_language(request):
         resp.headers = {'Location': request.referrer}
         resp.status = '302'
         resp.set_cookie('language', local_name)
+        a =b
         return resp
     else:
         return HTTPInternalServerError()
 
 def my_view(request):
-    return {'project':'SciELOWeb'}
+    return {'project':'scieloweb'}
     
 def sci_home(request):
+    settings = request.registry.settings
     locale_name = negotiate_locale_name(request) 
     localizer = get_localizer(request)
     
     main = get_renderer('scieloweb:templates/base.pt').implementation()
     
-    return {'settings': settings.WS_CONFIG,
-            'main': main}    
+    return {'settings': settings_app.WS_CONFIG,
+            'main': main,
+            '_': _ }    
 
 def sci_alphabetic(request):
-    query1    = urllib2.urlopen(settings.COUCHDB_VIEWS["sci_alphabetic"])
-    query2    = urllib2.urlopen(settings.COUCHDB_QUERIES["issues_count_all"])
+    settings = request.registry.settings
+    locale_name = negotiate_locale_name(request) 
+    localizer = get_localizer(request)
+    
+    query1    = urllib2.urlopen(settings_app.COUCHDB_VIEWS["sci_alphabetic"])
+    query2    = urllib2.urlopen(settings_app.COUCHDB_QUERIES["issues_count_all"])
     docjson   = json.loads(query1.read())
     iss_count = json.loads(query2.read())
     issues_total = {}
@@ -77,7 +83,7 @@ def sci_alphabetic(request):
     
     main = get_renderer('scieloweb:templates/base.pt').implementation()
     
-    return {'settings': settings.WS_CONFIG,
+    return {'settings': settings_app.WS_CONFIG,
             'document': {"rows":docf},
             'others': { "total_current": len(docf['current']),
                         "total_noncurrent": len(docf['noncurrent']),
@@ -85,14 +91,17 @@ def sci_alphabetic(request):
                        },
             'main': main}
 
-def sci_serial(request):  
+def sci_serial(request):
+    settings = request.registry.settings
+    locale_name = negotiate_locale_name(request) 
+    localizer = get_localizer(request)    
     pidval = request.matchdict['pid']
     issn_newtitle = ''
     new_title = ''
     
-    query1 = urllib2.urlopen(settings.COUCHDB_VIEWS["sci_serial"].format(pid=pidval))
-    query2 = urllib2.urlopen(settings.COUCHDB_QUERIES["journal_pressreleases"].format(pid=pidval,brac="{}"))
-    query3 = urllib2.urlopen(settings.COUCHDB_QUERIES["journal_lastarticles"].format(pid=pidval,brac="{}"))
+    query1 = urllib2.urlopen(settings_app.COUCHDB_VIEWS["sci_serial"].format(pid=pidval))
+    query2 = urllib2.urlopen(settings_app.COUCHDB_QUERIES["journal_pressreleases"].format(pid=pidval,brac="{}"))
+    query3 = urllib2.urlopen(settings_app.COUCHDB_QUERIES["journal_lastarticles"].format(pid=pidval,brac="{}"))
     
     serialjson = json.loads(query1.read());
     pressreleasesjson = json.loads(query2.read());
@@ -123,18 +132,21 @@ def sci_serial(request):
     
     main = get_renderer('scieloweb:templates/base.pt').implementation()
     
-    return {'settings': settings.WS_CONFIG,
+    return {'settings': settings_app.WS_CONFIG,
             'document': document,
             'main': main}
 
 def sci_issues(request):
+    settings = request.registry.settings
+    locale_name = negotiate_locale_name(request) 
+    localizer = get_localizer(request)    
     pidval = request.matchdict['pid']
     issues_dict = {}
     issn_newtitle = ''
     new_title = ''
     
-    query1 = urllib2.urlopen(settings.COUCHDB_VIEWS["sci_serial"].format(pid=pidval))
-    query2 = urllib2.urlopen(settings.COUCHDB_VIEWS["sci_issues"].format(pid=pidval,brac="{}"))
+    query1 = urllib2.urlopen(settings_app.COUCHDB_VIEWS["sci_serial"].format(pid=pidval))
+    query2 = urllib2.urlopen(settings_app.COUCHDB_VIEWS["sci_issues"].format(pid=pidval,brac="{}"))
     serialjson = json.loads(query1.read());
     issuesjson = json.loads(query2.read())
     
@@ -207,12 +219,15 @@ def sci_issues(request):
     
     main = get_renderer('scieloweb:templates/base.pt').implementation()
     
-    return {'settings': settings.WS_CONFIG,
+    return {'settings': settings_app.WS_CONFIG,
             'document': document,
             'main': main
             }
 
 def sci_issuetoc(request):
+    settings = request.registry.settings
+    locale_name = negotiate_locale_name(request) 
+    localizer = get_localizer(request)    
     pidval = request.matchdict['pid']
     issues_dict = {}
     issn_newtitle = ''
@@ -222,10 +237,12 @@ def sci_issuetoc(request):
     year = ''
     month = ''
     serial_sections = ''
+    title_translated = ''
+    article_default_language = locale_name
     
-    query1 = urllib2.urlopen(settings.COUCHDB_VIEWS["sci_serial"].format(pid=pidval[0:9]))
-    query2 = urllib2.urlopen(settings.COUCHDB_VIEWS["sci_issuetoc"].format(pid=pidval))
-    query3 = urllib2.urlopen(settings.COUCHDB_VIEWS["sci_issue"].format(pid=pidval[0:18]))
+    query1 = urllib2.urlopen(settings_app.COUCHDB_VIEWS["sci_serial"].format(pid=pidval[0:9]))
+    query2 = urllib2.urlopen(settings_app.COUCHDB_VIEWS["sci_issuetoc"].format(pid=pidval))
+    query3 = urllib2.urlopen(settings_app.COUCHDB_VIEWS["sci_issue"].format(pid=pidval[0:18]))
     serialjson = json.loads(query1.read())
     articlejson = json.loads(query2.read())
     issuejson =  json.loads(query3.read())
@@ -305,6 +322,25 @@ def sci_issuetoc(request):
 
         pid = rows['doc']['v880'][0]['_']
         
+        if rows['doc'].has_key('v12'):
+            titles = rows['doc']['v12']
+            
+            titles_dict = {}
+            for title in titles:
+                if titles_dict.has_key(title['l']):
+                    titles_dict[title['l']] = title['_']
+                else:
+                    titles_dict[title['l']] = ''
+                    titles_dict[title['l']] = title['_']
+
+            # getting title according to language
+            if titles_dict.has_key(locale_name):
+                title_translated = titles_dict[locale_name]
+            elif titles_dict.has_key(settings.default_locale_name):
+                title_translated = titles_dict[settings.default_locale_name]
+            else:
+                title_translated = article_default_language        
+        
         section = ''
         if rows['doc'].has_key('v49'):
             section = rows['doc']['v49'][0]['_']
@@ -313,7 +349,7 @@ def sci_issuetoc(request):
             articles[pid] = {}
             
         articles[pid] = { "pid": pid,
-                   "title": rows['doc']['v12'][0]['_'],
+                   "title": title_translated,
                "authors": authors_arr,
                "section": section
                }
@@ -340,12 +376,15 @@ def sci_issuetoc(request):
     
     main = get_renderer('scieloweb:templates/base.pt').implementation()
     
-    return {'settings': settings.WS_CONFIG,
+    return {'settings': settings_app.WS_CONFIG,
             'document': document,
             'main': main
             }
 
 def sci_arttext(request):
+    settings = request.registry.settings
+    locale_name = negotiate_locale_name(request) 
+    localizer = get_localizer(request)    
     pidval = request.matchdict['pid']
     issues_dict = {}
     issn_newtitle = ''
@@ -355,9 +394,13 @@ def sci_arttext(request):
     year = ''
     month = ''
     abstract = ''
+    article_default_language = locale_name
+    abstract_translated = ''
+    title_translated = ''
+    keyword_translated = ''
     
-    query1 = urllib2.urlopen(settings.COUCHDB_VIEWS["sci_serial"].format(pid=pidval[1:10]))
-    query2 = urllib2.urlopen(settings.COUCHDB_VIEWS["sci_article"].format(pid=pidval[0:23]))
+    query1 = urllib2.urlopen(settings_app.COUCHDB_VIEWS["sci_serial"].format(pid=pidval[1:10]))
+    query2 = urllib2.urlopen(settings_app.COUCHDB_VIEWS["sci_article"].format(pid=pidval[0:23]))
     serialjson = json.loads(query1.read())
     articlejson = json.loads(query2.read())
     
@@ -385,6 +428,11 @@ def sci_arttext(request):
     }
     
     # Creating article dict just with the relevant data for abstract page
+    pid = articlejson['rows'][0]['doc']['v880'][0]['_']
+        
+    if articlejson['rows'][0]['doc'].has_key('v40'):  #Article Default language
+        article_default_language = articlejson['rows'][0]['doc']['v40']
+    
     if articlejson['rows'][0]['doc'].has_key('v83'):  #Abstract
         abstracts = articlejson['rows'][0]['doc']['v83']
         
@@ -395,7 +443,15 @@ def sci_arttext(request):
             else:
                 abstracts_dict[abstract['l']] = ''
                 abstracts_dict[abstract['l']] = abstract['a']
-
+        
+        # getting abstract according to language
+        if abstracts_dict.has_key(locale_name):
+            abstract_translated = abstracts_dict[locale_name]
+        elif abstracts_dict.has_key(settings.default_locale_name):
+            abstract_translated = abstracts_dict[settings.default_locale_name]
+        else:
+            abstract_translated = article_default_language
+            
     if articlejson['rows'][0]['doc'].has_key('v85'):  #Keywords
         keywords = articlejson['rows'][0]['doc']['v85']
         
@@ -406,7 +462,15 @@ def sci_arttext(request):
                     keywords_dict[keyword['l']].append(keyword['k'])
                 else:
                     keywords_dict[keyword['l']] = []
-                    keywords_dict[keyword['l']].append(keyword['k'])        
+                    keywords_dict[keyword['l']].append(keyword['k'])
+                    
+        # getting keywords according to language
+        if keywords_dict.has_key(locale_name):
+            keyword_translated = keywords_dict[locale_name]
+        elif keywords_dict.has_key(settings.default_locale_name):
+            keyword_translated = keywords_dict[settings.default_locale_name]
+        else:
+            keyword_translated = article_default_language
 
     if articlejson['rows'][0]['doc'].has_key('v10'): #Authors
         authors = articlejson['rows'][0]['doc']['v10']
@@ -434,14 +498,22 @@ def sci_arttext(request):
                 titles_dict[title['l']] = ''
                 titles_dict[title['l']] = title['_']
 
-    pid = articlejson['rows'][0]['doc']['v880'][0]['_']
+        # getting title according to language
+        if titles_dict.has_key(locale_name):
+            title_translated = titles_dict[locale_name]
+        elif titles_dict.has_key(settings.default_locale_name):
+            title_translated = titles_dict[settings.default_locale_name]
+        else:
+            title_translated = article_default_language
+            
+
     
     article = { "pid": pid,
-                "abstract": abstracts_dict['en'],
-                "keywords": keywords_dict['en'],
+                "abstract": abstract_translated,
+                "keywords": keyword_translated,
                 "authors": authors_dict,
-                "title": titles_dict['en'],
-                "doi": settings.WS_CONFIG['identity']['doi_prefix']+'/'+pid
+                "title": title_translated,
+                "doi": settings_app.WS_CONFIG['identity']['doi_prefix']+'/'+pid
     }
     
     document = {    "serial": serial,
@@ -450,12 +522,15 @@ def sci_arttext(request):
     
     main = get_renderer('scieloweb:templates/base.pt').implementation()
     
-    return {'settings': settings.WS_CONFIG,
+    return {'settings': settings_app.WS_CONFIG,
             'document': document,
             'main': main
             }
 
 def sci_abstract(request):
+    settings = request.registry.settings
+    locale_name = negotiate_locale_name(request) 
+    localizer = get_localizer(request)    
     pidval = request.matchdict['pid']
     issues_dict = {}
     issn_newtitle = ''
@@ -465,9 +540,13 @@ def sci_abstract(request):
     year = ''
     month = ''
     abstract = ''
+    article_default_language = locale_name
+    abstract_translated = ''
+    title_translated = ''
+    keyword_translated = ''
     
-    query1 = urllib2.urlopen(settings.COUCHDB_VIEWS["sci_serial"].format(pid=pidval[1:10]))
-    query2 = urllib2.urlopen(settings.COUCHDB_VIEWS["sci_article"].format(pid=pidval[0:23]))
+    query1 = urllib2.urlopen(settings_app.COUCHDB_VIEWS["sci_serial"].format(pid=pidval[1:10]))
+    query2 = urllib2.urlopen(settings_app.COUCHDB_VIEWS["sci_article"].format(pid=pidval[0:23]))
     serialjson = json.loads(query1.read())
     articlejson = json.loads(query2.read())
     
@@ -495,6 +574,11 @@ def sci_abstract(request):
     }
     
     # Creating article dict just with the relevant data for abstract page
+    pid = articlejson['rows'][0]['doc']['v880'][0]['_']
+        
+    if articlejson['rows'][0]['doc'].has_key('v40'):  #Article Default language
+        article_default_language = articlejson['rows'][0]['doc']['v40']
+    
     if articlejson['rows'][0]['doc'].has_key('v83'):  #Abstract
         abstracts = articlejson['rows'][0]['doc']['v83']
         
@@ -505,7 +589,15 @@ def sci_abstract(request):
             else:
                 abstracts_dict[abstract['l']] = ''
                 abstracts_dict[abstract['l']] = abstract['a']
-
+        
+        # getting abstract according to language
+        if abstracts_dict.has_key(locale_name):
+            abstract_translated = abstracts_dict[locale_name]
+        elif abstracts_dict.has_key(settings.default_locale_name):
+            abstract_translated = abstracts_dict[settings.default_locale_name]
+        else:
+            abstract_translated = article_default_language
+            
     if articlejson['rows'][0]['doc'].has_key('v85'):  #Keywords
         keywords = articlejson['rows'][0]['doc']['v85']
         
@@ -516,7 +608,15 @@ def sci_abstract(request):
                     keywords_dict[keyword['l']].append(keyword['k'])
                 else:
                     keywords_dict[keyword['l']] = []
-                    keywords_dict[keyword['l']].append(keyword['k'])        
+                    keywords_dict[keyword['l']].append(keyword['k'])
+                    
+        # getting keywords according to language
+        if keywords_dict.has_key(locale_name):
+            keyword_translated = keywords_dict[locale_name]
+        elif abstracts_dict.has_key(settings.default_locale_name):
+            keyword_translated = keywords_dict[settings.default_locale_name]
+        else:
+            keyword_translated = article_default_language
 
     if articlejson['rows'][0]['doc'].has_key('v10'): #Authors
         authors = articlejson['rows'][0]['doc']['v10']
@@ -544,14 +644,22 @@ def sci_abstract(request):
                 titles_dict[title['l']] = ''
                 titles_dict[title['l']] = title['_']
 
-    pid = articlejson['rows'][0]['doc']['v880'][0]['_']
+        # getting title according to language
+        if titles_dict.has_key(locale_name):
+            title_translated = titles_dict[locale_name]
+        elif abstracts_dict.has_key(settings.default_locale_name):
+            title_translated = titles_dict[settings.default_locale_name]
+        else:
+            title_translated = article_default_language
+            
+
     
     article = { "pid": pid,
-                "abstract": abstracts_dict['en'],
-                "keywords": keywords_dict['en'],
+                "abstract": abstract_translated,
+                "keywords": keyword_translated,
                 "authors": authors_dict,
-                "title": titles_dict['en'],
-                "doi": settings.WS_CONFIG['identity']['doi_prefix']+'/'+pid
+                "title": title_translated,
+                "doi": settings_app.WS_CONFIG['identity']['doi_prefix']+'/'+pid
     }
     
     document = {    "serial": serial,
@@ -560,7 +668,7 @@ def sci_abstract(request):
     
     main = get_renderer('scieloweb:templates/base.pt').implementation()
     
-    return {'settings': settings.WS_CONFIG,
+    return {'settings': settings_app.WS_CONFIG,
             'document': document,
             'main': main
             }
