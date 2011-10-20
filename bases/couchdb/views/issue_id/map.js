3edb2836e3043e85148ba4570fed02331fcc372d
Fabio Batalha C Santos <fabiobatalha@gmail.com>
2011-10-20 11:31:40 -0200

    Including new view to separate press releases issues from article issues

--- bases/couchdb/views/issue_id/map.js
+++ bases/couchdb/views/issue_id/map.js
@@ -1,7 +1,10 @@
 function(doc) {
+function(doc) {
   // !code _attachments/js/includes.js
-
- if (doc.v706[0]["_"] == 'i'){
-     emit(doc.v35[0]["_"]+doc.v36[0]["_"].substr(0,4)+PadDigits(doc.v36[0]["_"].substr(4),4), null);
-  }
+ 
+ if (doc.v41[0]["_"] != 'pr'){
+     if (doc.v706[0]["_"] == 'i'){
+         emit(doc.v35[0]["_"]+doc.v36[0]["_"].substr(0,4)+PadDigits(doc.v36[0]["_"].substr(4),4), null);
+     }
+ }
 }
