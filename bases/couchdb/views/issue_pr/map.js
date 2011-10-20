247f595f0285450373a6e9e18295e4677f697e9f
Fabio Batalha C Santos <fabiobatalha@gmail.com>
2011-10-20 11:59:03 -0200

    fixing v41 validation to exclude press releases from issue_id view

--- bases/couchdb/views/issue_pr/map.js
+++ bases/couchdb/views/issue_pr/map.js
@@ -2,7 +2,7 @@ function(doc) {
 function(doc) {
   // !code _attachments/js/includes.js
  
- if (doc.v41[0]["_"] == undefined){
+ if (doc.v41[0]["_"] == 'pr'){
      if (doc.v706[0]["_"] == 'i'){
          emit(doc.v35[0]["_"]+doc.v36[0]["_"].substr(0,4)+PadDigits(doc.v36[0]["_"].substr(4),4), null);
      }
